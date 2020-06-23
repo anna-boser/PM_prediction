6aa6a72ade9a05402737018a0a05f7a6d810025c
Anna Boser <annaboser@berkeley.edu>
2020-06-23 11:19:07 -0700

    more data stuff

--- Code/Scrapers/download_NED.R
+++
@@ -1,5 +0,0 @@
-#Download National Elevation data
-
-dir <- "~/GitHub/PM_prediction/Data/NED/"
-
-download.file(url = '???', destfile = paste0(dir, "NED.zip"))
---
+++ Code/Scrapers/get_NED_and_TIGER.R
@@ -0,0 +1,8 @@
+#get NED (elevation) and TIGER (roads) Data
+
+#Go to https://gdg.sc.egov.usda.gov//
+#define your long/lat bounding box: https://gdg.sc.egov.usda.gov//GDGOrder.aspx?order=MBROrder
+#select NED and TIGER data
+#have it sent to your email and then download it
+#store NED under ~/Documents/GitHub/PM_prediction/Data/NED
+#Store TIGER under ~/Documents/GitHub/PM_prediction/Data/TIGER
